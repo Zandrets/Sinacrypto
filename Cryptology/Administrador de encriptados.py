ffcc62611f6ad7965db9f85395be7286cbcecb91
zandrets <101905998+zandrets@users.noreply.github.com>
2022-03-24 00:01:39 -0600

    Adding settings.ini & updating AES encrypt mode
    Added settings.ini file
    updated AES encrypt function with names generation and starting security for the file and key

--- Cryptology/Administrador de encriptados.py
+++ Cryptology/Administrador de encriptados.py
@@ -1,7 +1,9 @@
-import Functions as tools
+#import Functions as tools
 import os
-import AES_256_sample as AES256
 from time import time, ctime, sleep
+from random import randbytes as rng
+import configparser
+import AES_256_sample as AES256
 import MD5_sample
 
 #KEYS ALGORITHMS, you can add here your function
@@ -13,37 +15,42 @@ def AES(msg, method, msg_type, mode):
         open(os.getcwd()+'//'+'encrypt_system.log', 'at').write(log)
 
         #Preconfig section
+        configs=configparser.ConfigParser()
+        configs.read('settings.ini')
+        tag=configs['ENCRYPT']['TAG']
         if mode == 256 or mode == 128 or mode == 64:
             start_time=time()                       #Start time
             key=AES256.generate_key(mode/8)         #Generates an aleatory key
         else:   return 1                            #Fail reason invalid mode or none one
         if msg_type == "file":
+            name=os.path.basename(msg)
+            path=os.path.dirname(msg)
             content=open(msg, 'rb').read()          #Open and read the file in binary mode
         elif msg_type == "text":
+            name=MD5_sample.encrypt(rng(32))
+            path=os.getcwd()+'/tmp'
             content=bytes(msg, 'utf-8')             #Change the message to binary
         else :  return 2                            #Fail reason, invalid message type or none one
 
         #Encrypt section
         print("encrypting...")
         encrypt=AES256.encrypt(key,content)                                                          #Keep the encrypted data
+        open(path+'/'+MD5_sample.encrypt(bytes(log, 'utf-8')), '+wb').write(name)                   #Overwrite the file with the encrypted data
+        open(path+'/'+MD5_sample.encrypt(bytes(log, 'utf-8')), 'ab').write(tag)
+        open(path+'/'+MD5_sample.encrypt(bytes(log, 'utf-8')), 'ab').write(encrypt)
 
         #Export section
-        if msg_type == "file":
-            output_file=msg
-            open(msg, '+wb').write(encrypt)                                                          #Overwrite the file with the encrypted data
-        elif msg_type == "text":
-            output_file=os.getcwd()+'\\'+MD5_sample.encrypt(bytes(log_start_time, 'utf-8'))+'.tmp'   #Temporal solution, make a new file with the encrypted data, pending to update to multiple OS
-            open(output_file, '+wb').write(encrypt)
-        else :  return 3
-        print("exporting key...")
-        open(os.getcwd()+'\\'+MD5_sample.encrypt(bytes(os.path.getctime(output_file)), 'utf-8')+'.key', '+wb').write(key)
-
-        #Log section
         log_end_time=ctime(time())
         if log_end_time == log_start_time: sleep(1); log_end_time=ctime(time())
         log="\n ending AES %d encrypt at %s" % (mode, log_end_time)
+        os.remove(msg)
+        print("exporting key...")
+        open(os.getcwd()+'/tmp/'+MD5_sample.encrypt(log, 'utf-8')+'.key', '+wb').write(key)
+
+        #End section
         open(os.getcwd()+'//'+'encrypt_system.log', 'at').write(log)
         return 0
+
         #CAMBIA EL NOMBRE DEL ARCHIVO CON UN HASH DEL MISMO, BUSCA COMO GUARDAR DICHO NOMBRE Y USA COMO PUNTO ORIGEN EL TIEMPO DE INICIO Y EL TAMANO EN EL LOG
 
     elif method== "decrypt":
@@ -55,4 +62,4 @@ if __name__ == '__main__':
     #ENCRYPT USAGE
 #    print(AES(str(path)+'\\'+file,'encrypt','file',256))      #for files
 #    print(AES('text to be encrypted', 'encrypt','text',256))  #for text
-    print(AES(''))
+#    print(AES(''))
--- Cryptology/test_script.py
+++ Cryptology/test_script.py
@@ -1,14 +1,25 @@
-#from random import randbytes
-#print(len(randbytes(32)))
-#file=open("texto de leer.txt", "rt").read()
-#file.close()
-#print(file)
-#from time import time, ctime
-
-#tiempo=time()
-#print(type(ctime(tiempo)))
 import os
-from time import ctime
-import time
-#print(ctime(os.path.getmtime('test_file_2.txt')))
-print(time.gmtime())
+import configparser
+from time import time, ctime
+import random
+import MD5_sample
+mode=256
+print(os.path.dirname(os.path.abspath('ECC_sample.py')))
+print(open('c:\\users\\MC17060010.WMX/Downloads\\Sinacrypto-main\\Cryptology/texto de leer.txt', 'rt').read())
+configs=configparser.ConfigParser()
+configs.read('settings.ini')
+tag=configs['ENCRYPT']['TAG']
+print(__file__)
+print(tag)
+name=a=1
+print(MD5_sample.encrypt(random.randbytes(32)))
+#log_start_time=ctime(time())
+#log="\n starting AES %d encrypt at %s" % (mode, log_start_time)
+#open(os.getcwd()+'/'+'encrypt_system.log', 'at').write(log)
+print(os.getcwd()+'/tmp')
+#open('asdf.tmp', '+wt').write('123')
+print(ctime(os.path.getctime('asdf.tmp')))
+
+#print()
+#if 'abcde' ~= 'abc':
+    
