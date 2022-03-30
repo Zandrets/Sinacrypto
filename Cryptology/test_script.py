943288adef83f3549f1c39e2ff4769158eeb1006
zandrets <101905998+zandrets@users.noreply.github.com>
2022-03-30 00:21:01 -0600

    Update test_script.py
    Just tested some functions

--- Cryptology/test_script.py
+++ Cryptology/test_script.py
@@ -1,16 +1,18 @@
-# import os
-# import configparser
-# from time import time, ctime
-# import random
-# import MD5_sample
-# import re
-# import AES_256_sample as AES
+import os
+import configparser
+import pandas as pd
+import Functions as tools
+from time import time, ctime
+import random
+import MD5_sample
+import re
+import AES_256_sample as AES
 # mode=256
 # print(os.path.dirname(os.path.abspath('ECC_sample.py')))
 # #print(open('c:\\users\\MC17060010.WMX/Downloads\\Sinacrypto-main\\Cryptology/texto de leer.txt', 'rt').read())
-# configs=configparser.ConfigParser()
-# configs.read(os.path.dirname(__file__)+'/'+'settings.ini')
-# tag=configs['ENCRYPT']['TAG']
+configs=configparser.ConfigParser()
+configs.read(os.path.dirname(__file__)+'/'+'settings.ini')
+tag=bytes(configs['ENCRYPT']['TAG'], 'utf-8')
 # print(__file__)
 # print(tag)
 # name=a=1
@@ -30,10 +32,36 @@
 # exit()
 # print(type(configs['ENCRYPT']['FILE_TYPE']))
 # if re.search('axc', axy):
-#     print('encontrado')    
-import pandas as pd
-import os
-llaves=pd.DataFrame(pd.read_csv(os.path.dirname(os.path.dirname(__file__))+'/'+'sistemas_llaves.csv'))
-print(llaves)
-for i in range(1, 3):
-    print(i)
+#     print('encontrado')
+# #llaves=pd.DataFrame(pd.read_csv(os.path.dirname(os.path.dirname(__file__))+'/'+'sistemas_llaves.csv'))
+#print(llaves)
+#for i in range(1, 3):
+#    print(i)
+
+#class TEST():
+#    def __init__(self, *args):
+#        print(args[0])
+#TEST('a', 'b', 'c')
+
+open('tmp_file.rar', '+wb').write(bytes('nombre_de_prueba.rar', 'utf-8')+tag+open('test_file.rar', 'rb').read())
+raw_content=open('tmp_file.rar', 'rb').read()
+size=tools.file_reader('./','test_file.rar')
+new_size=tools.file_reader('./','tmp_file.rar')
+name=''
+for i in range(0, new_size):
+    s_tag=''
+    if chr(raw_content[i]) == chr(tag[0]):
+        for j in range(0, len(tag)):
+            s_tag=s_tag+chr(raw_content[i+j])
+    if s_tag==tag.decode('utf-8'):
+        content=b''
+        print(chr(raw_content[i+len(tag)]))
+        for k in range((i+len(tag)), new_size):
+            content=content+bytes([raw_content[k]])
+        print((i+len(tag)), size)
+        break
+    name=name+chr(raw_content[i])
+open(name, '+wb').write(content)
+print(tools.file_reader('./','test_file.rar') )
+print(tools.file_reader('./',name))
+#print(name)
